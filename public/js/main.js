520df8980bf6c390942a0f6bd3c7ac9a7aca2c1a
Life1223pl <daw_pl@interia.pl>
2021-11-01 17:41:19 +0100

    Second Commit

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,1 @@
-const articles = document.getElementById('articles');
 
-if(articles){
-    articles.addEventListener('dblclick', e => {
-       if(e.target.className === 'btn btn-danger delete-article'){
-           alert("2");
-       }
-    });
-}
